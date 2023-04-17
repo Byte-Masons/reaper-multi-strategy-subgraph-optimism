10bf18179fbd086e64a8b6086970ff9a2bccfefe
tess3rac7 <tess3rac7@protonmail.com>
2023-04-17 15:22:22 -0400

    Add zok to potential strategy-adder list.

--- src/vault-indexer.ts
+++ src/vault-indexer.ts
@@ -6,6 +6,7 @@ import { ReaperVaultERC4626 } from "../generated/templates";
 const addStrategyAddresses: Address[] = [
   Address.fromString("0x04C710a1E8a738CDf7cAD3a52Ba77A784C35d8CE"), // super-admin multisig
   Address.fromString("0xe1610bB38Ce95254dD77cbC82F9c1148569B560e"), // goober
+  Address.fromString("0x950b0E7FD95a08C9525bC82AaE0A8121cC84143E"), // zokunei
   // ... add other addresses that may call addStrategy on a new or existing vault 
 ];
 
